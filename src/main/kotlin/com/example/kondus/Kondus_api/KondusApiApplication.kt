81246067a5f516265ce7900f3ac3b2b410455120
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-08-27 19:37:22 -0300

    init

---
+++ src/main/kotlin/com/example/kondus/Kondus_api/KondusApiApplication.kt
@@ -0,0 +1,20 @@
+package com.example.kondus.Kondus_api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@SpringBootApplication
+class KondusApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<KondusApiApplication>(*args)
+}
+
+@RestController
+class HelloController{
+
+	@GetMapping("/")
+	fun hello() = "Hello"
+}
