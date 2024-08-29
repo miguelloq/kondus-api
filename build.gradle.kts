d62bb8a6ab640b3eb5c8f7d75dbc16b19f148c59
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-08-28 21:03:28 -0300

    auth dependecies in build.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,7 +29,6 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.flywaydb:flyway-core")
@@ -41,13 +40,21 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-	testImplementation("org.springframework.security:spring-security-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+	//SPRING SECURITY
+	implementation("org.springframework.boot:spring-boot-starter-security")
+	testImplementation("org.springframework.security:spring-security-test")
 
 	//FP
 	implementation(platform("io.arrow-kt:arrow-stack:1.2.4"))
 	implementation("io.arrow-kt:arrow-core")
 	implementation("io.arrow-kt:arrow-fx-coroutines")
+
+	//JJWT
+	implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+	implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 kotlin {
