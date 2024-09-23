08c84700b2eb774fb29ae96b01f05140db378739
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-09-22 21:34:53 -0300

    Users table migration

---
+++ src/main/resources/db/migration/V1__create_role_enum_and_users_table.sql
@@ -0,0 +1,8 @@
+create TYPE role as enum ('ADMIN', 'DEFAULT');
+
+create table users (
+    id serial primary key,
+    email text not null,
+    password text not null,
+    role role not null default 'DEFAULT'
+);
