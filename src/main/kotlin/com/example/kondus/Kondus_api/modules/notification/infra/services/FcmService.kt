51f164ea61d82a6ef9ae3061283121fd526a0369
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-09-30 14:28:55 -0300

    Refactoring FcmService to a more expressive approach

--- src/main/kotlin/com/example/kondus/Kondus_api/modules/notification/infra/services/FcmService.kt
+++ src/main/kotlin/com/example/kondus/Kondus_api/modules/notification/infra/services/FcmService.kt
@@ -10,55 +10,70 @@ import com.google.firebase.messaging.Message
 import com.google.gson.GsonBuilder
 import org.springframework.stereotype.Service
 import java.time.Duration
-import java.util.concurrent.ExecutionException
 import com.google.firebase.messaging.Notification
 
 @Service
 class FCMService {
-
     fun sendMessageToToken(request: NotificationRequestModel) = try{
-        val message = getPreconfiguredMessageToToken(request)
-        val gson = GsonBuilder().setPrettyPrinting().create()
-        val jsonOutput = gson.toJson(message)
-        sendAndGetResponse(message)
+        request
+            .toMessage()
+            .also{print("Sending message: ${it.toGson()}")}
+            .let{sendMessage(it)}
+            .also{print("Send operation operation response:$it")}
         Unit
     } catch(e:Exception){
         print("Fail to send $request \n because $e")
     }
 
-    private fun sendAndGetResponse(message: Message): String {
-        return FirebaseMessaging.getInstance().sendAsync(message).get()
-    }
+    private fun sendMessage(message: Message) : String =
+        FirebaseMessaging
+            .getInstance()
+            .sendAsync(message)
+            .get()
 
-    private fun getAndroidConfig(topic: String): AndroidConfig {
-        return AndroidConfig.builder()
+    private fun Message.toGson() : String  =
+        GsonBuilder()
+            .setPrettyPrinting()
+            .create()
+            .toJson(this)
+
+    private fun NotificationRequestModel.toMessage() : Message =
+        Message
+            .builder()
+            .setApnsConfig(getApnsConfig(topic))
+            .setAndroidConfig(getAndroidConfig(topic))
+            .setNotification(toNotification())
+            .setToken(token)
+            .build()
+
+    private fun NotificationRequestModel.toNotification() : Notification =
+        Notification
+            .builder()
+            .setTitle(title)
+            .setBody(body)
+            .build()
+
+    private fun getAndroidConfig(topic: String): AndroidConfig =
+        AndroidConfig
+            .builder()
             .setTtl(Duration.ofMinutes(2).toMillis())
             .setCollapseKey(topic)
             .setPriority(AndroidConfig.Priority.HIGH)
-            .setNotification(AndroidNotification.builder().setTag(topic).build())
+            .setNotification(AndroidNotification
+                .builder()
+                .setTag(topic)
+                .build())
             .build()
-    }
 
-    private fun getApnsConfig(topic: String): ApnsConfig {
-        return ApnsConfig.builder()
-            .setAps(Aps.builder().setCategory(topic).setThreadId(topic).build())
+
+    private fun getApnsConfig(topic: String): ApnsConfig =
+        ApnsConfig
+            .builder()
+            .setAps(Aps
+                    .builder()
+                    .setCategory(topic)
+                    .setThreadId(topic)
+                    .build()
+            )
             .build()
-    }
-
-    private fun getPreconfiguredMessageToToken(request: NotificationRequestModel): Message {
-        return getPreconfiguredMessageBuilder(request).setToken(request.token).build()
-    }
-
-    private fun getPreconfiguredMessageBuilder(request: NotificationRequestModel): Message.Builder {
-        val androidConfig = getAndroidConfig(request.topic)
-        val apnsConfig = getApnsConfig(request.topic)
-        val notification = Notification.builder()
-            .setTitle(request.title)
-            .setBody(request.body)
-            .build()
-        return Message.builder()
-            .setApnsConfig(apnsConfig)
-            .setAndroidConfig(androidConfig)
-            .setNotification(notification)
-    }
 }
