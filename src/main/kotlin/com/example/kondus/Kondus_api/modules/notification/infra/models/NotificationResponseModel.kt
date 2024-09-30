61ce36bc07521631455ac9351e0c46ec24f045fa
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-09-30 13:53:31 -0300

    Deleting NotificationResponse

--- src/main/kotlin/com/example/kondus/Kondus_api/modules/notification/infra/models/NotificationResponseModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.kondus.Kondus_api.modules.notification.infra.models
-
-import org.springframework.http.HttpStatus
-
-data class NotificationResponseModel(
-    val status: HttpStatus,
-    val message: String,
-)
