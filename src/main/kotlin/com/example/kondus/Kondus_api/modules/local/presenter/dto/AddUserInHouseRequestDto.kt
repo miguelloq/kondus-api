5957d19401657b8a52fff01088c5842fdcb56b5a
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-10-11 17:25:51 -0300

    Id cannot be null in HouseResponseDto and AddUserDto

---
+++ src/main/kotlin/com/example/kondus/Kondus_api/modules/local/presenter/dto/AddUserInHouseRequestDto.kt
@@ -0,0 +1,5 @@
+package com.example.kondus.Kondus_api.modules.local.presenter.dto
+
+data class AddUserInHouseRequestDto (
+    val houseId:Long?
+)
--- src/main/kotlin/com/example/kondus/Kondus_api/modules/local/presenter/dto/HouseResponseDto.kt
+++ src/main/kotlin/com/example/kondus/Kondus_api/modules/local/presenter/dto/HouseResponseDto.kt
@@ -1,7 +1,7 @@
 package com.example.kondus.Kondus_api.modules.local.presenter.dto
 
 data class HouseResponseDto(
-    val id:Long?=null,
+    val id:Long,
     val localId:Long,
     val description:String,
     val type:String,
