362bb4c167e87c7d42e7f5af27a80755ec3b6044
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2024-10-15 19:13:09 -0300

    Syntax error in V3__create_registrations_and_items_table

--- src/main/resources/db/migration/V3__create_registrations_and_items_table.sql
+++ src/main/resources/db/migration/V3__create_registrations_and_items_table.sql
@@ -5,14 +5,14 @@ create table items(
     category varchar(255) not null,
     price real,
     foreign key (user_id) references users(id) on delete cascade
-)
+);
 
 create table registrations(
     id serial primary key,
     item_id serial not null,
     dealer_id serial not null,
-    first_time timestamp not null
+    first_time timestamp not null,
     foreign key (item_id) references items(id) on delete cascade,
     foreign key (dealer_id) references users(id) on delete cascade
-)
+);
 
